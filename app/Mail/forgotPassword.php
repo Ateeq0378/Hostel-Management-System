42ee541343a007df8759e3cf866011e94f3da4b6
Ateeq Ahmad <120200619+Ateeq0378@users.noreply.github.com>
2025-10-26 14:36:37 +0530

    Create forgotPassword.php

---
+++ app/Mail/forgotPassword.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class forgotPassword extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $subject;
+    public $name;
+    public $email;
+    public $password;
+
+    public function __construct($subject, $name, $email, $password)
+    {
+        $this->subject = $subject;
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: $this->subject,
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.forgotPassword',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
