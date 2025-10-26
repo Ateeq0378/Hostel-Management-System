7fe9161a18887802aa8ea12afae523f3128cbb59
Ateeq Ahmad <120200619+Ateeq0378@users.noreply.github.com>
2025-10-26 14:35:00 +0530

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use App\Mail\forgotPassword;
 
 class UserController extends Controller
 {
@@ -85,6 +87,48 @@ class UserController extends Controller
         }
         else {
             return redirect()->route('student-dashboard')->with(['success' => 'Password updated successfully']);
+        }
+
+    }
+
+    function generatePassword() {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()';
+        return substr(str_shuffle($characters), 0, 8);
+    }
+
+    public function forgotPassword(Request $request){
+
+        $user = User::where('email', $request->email)->first();
+        $password = $this->generatePassword();
+        $user->password = $password;
+        $user->save();
+        $toemail = "ateeq0378@gmail.com";
+
+        Mail::to($toemail)->send(new forgotPassword(
+            'Forgot Password',
+            $user->name,
+            $user->email,
+            $password
+        ));
+
+        if($user->role == "provost" || $user->role == "warden"){
+            return redirect()->route('admin-dashboard')->with(['success' => 'A new password has been send to your email.']);
+        }
+        else {
+            return redirect()->route('student-dashboard')->with(['success' => 'A new password has been send to your email.']);
+        }
+
+    }
+
+    public function showDashboard(Request $request){
+
+        $user = User::where('email', $request->email)->first();
+
+        if($user->role == "provost" || $user->role == "warden"){
+            return view('admin.dashboard');
+        }
+        else {
+            return view('student.dashboard');
         }
 
     }
