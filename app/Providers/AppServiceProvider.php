55b9b9b956c1764193132a360a95ff1fd18b904a
Ateeq Ahmad <120200619+Ateeq0378@users.noreply.github.com>
2025-10-28 22:39:09 +0530

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\URL;
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
@@ -20,8 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') !== 'local') {
-            URL::forceScheme('https');
-        }
+        //
     }
 }
