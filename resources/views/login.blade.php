9f7bf123691968c109d4eb1fc722a36e37636ff9
Ateeq Ahmad <120200619+Ateeq0378@users.noreply.github.com>
2025-10-28 16:37:53 +0530

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,23 +10,23 @@
         <meta content="" name="keywords">
 
         <!-- Favicons -->
-        <link href="{{ asset('/assets/img/image.png') }}" rel="icon">
+        <link href="assets/img/image.png" rel="icon">
 
         <!-- Google Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
         <!-- Vendor CSS Files -->
-        <link href="{{ asset('/assets/vendor/aos/aos.css') }}" rel="stylesheet">
-        <link href="{{ asset('/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-        <link href="{{ asset('/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-        <link href="{{ asset('/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-        <link href="{{ asset('/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-        <link href="{{ asset('/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-        <link href="{{ asset('/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+        <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+        <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+        <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+        <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+        <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+        <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
         <!-- CSS File -->
-        <link href="{{ asset('/assets/css/main.css') }}" rel="stylesheet">
-        <link href="{{ asset('/assets/css/log-in.css') }}" rel="stylesheet">
+        <link href="assets/css/main.css" rel="stylesheet">
+        <link href="assets/css/log-in.css" rel="stylesheet">
 
     </head>
 
