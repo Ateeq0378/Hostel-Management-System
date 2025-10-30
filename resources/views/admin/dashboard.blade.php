ce4efe3959d9b5e811f0a71f95edf7c1ee5491b1
Ateeq Ahmad <120200619+Ateeq0378@users.noreply.github.com>
2025-10-30 13:05:44 +0530

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,6 +5,20 @@
 @endsection
 
 @section('content')
+
+@php
+    use App\Models\Room;
+    use App\Models\Student;
+    use App\Models\Warden;
+    use App\Models\Notice;
+    use App\Models\Complain;
+
+    $warden = Warden::count();
+    $student = Student::count();
+    $room = Room::count();
+    $notice = Notice::count();
+    $complain = Complain::count();
+@endphp
     
     <!-- ======= Dashbord ======= -->
 
@@ -23,7 +37,7 @@
                         <i class="fa-solid fa-user"></i>&nbsp Warden</a>
                     </h4>
                     <hr>
-                    <p class="card-text"><strong></strong></p>
+                    <p class="card-text"><strong>{{$warden}}</strong></p>
                 </div>
             </div>
         @endif
@@ -33,7 +47,7 @@
                     <i class="fa-solid fa-users"></i></i> &nbsp Student</a>
                 </h4>
                 <hr>
-                <p class="card-text"><strong></strong></p>
+                <p class="card-text"><strong>{{$student}}</strong></p>
             </div>
         </div>
         <div class="col-md-2 m-2 shadow-lg bg-white cards">
@@ -42,7 +56,7 @@
                     <i class="fa-regular fa-building"></i></i> &nbsp Room</a>
                 </h4>
                 <hr>
-                <p class="card-text"><strong></strong></p>
+                <p class="card-text"><strong>{{$room}}</strong></p>
             </div>
         </div>
         <div class="col-md-2 m-2 shadow-lg bg-white cards">
@@ -51,7 +65,7 @@
                     <i class="fa-solid fa-list-ul"></i> &nbsp Notice</a>
                 </h4>
                 <hr>
-                <p class="card-text"><strong></strong></p>
+                <p class="card-text"><strong>{{$notice}}</strong></p>
             </div>
         </div>
         <div class="col-md-2 m-2 shadow-lg bg-white cards">
@@ -60,7 +74,7 @@
                     <i class="fa-solid fa-list-ul"></i> &nbsp Complain</a>
                 </h4>
                 <hr>
-                <p class="card-text"><strong></strong></p>
+                <p class="card-text"><strong>{{$complain}}</strong></p>
             </div>
         </div>
     </div>
